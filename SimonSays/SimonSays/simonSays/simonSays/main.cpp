d0634afc3e47429d9a71db1f82fdca1c610a31cc
(Student) - Emma Mc Donald <C00271718@itcarlow.ie>
2022-01-28 12:35:18 +0000

    changed name

--- SimonSays/SimonSays/simonSays/simonSays/main.cpp
+++ SimonSays/SimonSays/simonSays/simonSays/main.cpp
@@ -1,8 +1,8 @@
 /// <summary>
 /// simple game loop for SFML[2.5.1]
 /// 
-/// @author Peter Lowe
-/// @date 21 jan
+/// @author Emma McDonald
+/// @date fri 28 2022
 /// </summary>
 
 #ifdef _DEBUG 
